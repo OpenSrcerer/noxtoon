fe99df2dc79c421e4d6a3b2d55a03865930a1896
OpenSrcerer <daniel.stefani@crispybacon.al>
2023-03-10 22:16:22 +0200

    patch: move css to public

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import './assets/css/main.css'
+import '../public/css/main.css'
 import {initializeFirebase} from "@/services/FirebaseClient";
 import router from "@/router";
 
