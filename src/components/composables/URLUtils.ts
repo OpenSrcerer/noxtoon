f70a0e4ae4c9ada135b76e197c6d8b85ed469830
OpenSrcerer <daniel.stefani@crispybacon.al>
2023-03-12 14:48:17 +0200

    patch: fix URLUtils glob

--- src/components/composables/URLUtils.ts
+++ src/components/composables/URLUtils.ts
@@ -1,4 +1,4 @@
 export const getDynamicImage = (imageName: string, nestedPath= "") => {
-    const path = new URL(`@/assets/img/${nestedPath}`, import.meta.url);
+    const path = new URL('@/assets/img/' + nestedPath, import.meta.url);
     return `${path.pathname}/${imageName}.png`.replace("/@", "")
 }
