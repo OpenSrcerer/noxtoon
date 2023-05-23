087d526b7b8dbd6be9a2ad2e30d349dbe28073ee
Daniel Stefani <daniel.stefani@crispybacon.al>
2023-05-23 18:52:02 +0300

    [ci skip] feat: add user create APIs

--- backend/src/controllers/users/post_user.php
+++ backend/src/controllers/users/post_user.php
@@ -1,2 +1,27 @@
 <?php
+
+namespace models;
+
 include "../../common/response_headers.php";
+include "../../repositories/user_repository.php";
+
+if (
+    !isset($_POST['username']) ||
+    !isset($_POST['password'])
+) {
+    http_response_code(400);
+    exit;
+}
+
+$create_user_success = create_user(
+    $_POST['username'],
+    $_POST['password']
+);
+
+if (!$create_user_success) {
+    http_response_code(409);
+    exit;
+}
+
+http_response_code(201);
+// echo json_encode(get_users());
--- backend/src/repositories/user_repository.php
+++ backend/src/repositories/user_repository.php
@@ -1,16 +1,62 @@
 <?php
 
-function create_comment(
-    string $content,
-    string $date,
-    string $cartoon_id,
-    string $user_id
-): void {
+use models\User;
+
+include "../../models/User.php";
+include "../../db/db_utils.php";
+include "../../utils/uuid_generator.php";
+
+function create_user(
+    string $username,
+    string $password,
+): bool {
+    if (is_username_taken($username)) {
+        return false;
+    }
+
+    $generated_uuid = generate_uuidv4();
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
     $sql =<<<EOF
-        INSERT INTO comments (user_id, content, post_date, cartoon_id)
-        VALUES ('$user_id', '$content', '$date', '$cartoon_id')
+        INSERT INTO users (id, username, password_hash)
+        VALUES ('$generated_uuid', '$username', '$hashed_password')
     EOF;
 
     [$db,] = exec_sql($sql);
     pg_close($db);
+
+    return true;
+}
+
+//function get_users(): array
+//{
+//    $users = [];
+//    $sql =<<<EOF
+//        SELECT * FROM users;
+//    EOF;
+//
+//    [$db, $res] = exec_sql($sql);
+//    while ($row = pg_fetch_row($res)) {
+//        $users[] = User::from_row($row);
+//    }
+//    pg_close($db);
+//
+//    return $users;
+//}
+
+function is_username_taken(
+    string $username
+): bool {
+    $sql =<<<EOF
+        SELECT COUNT(*) FROM users WHERE username = '$username'
+    EOF;
+
+    [$db, $res] = exec_sql($sql);
+    $number_of_users_with_username = pg_fetch_row($res)[0];
+
+    pg_close($db);
+
+    if ($number_of_users_with_username > 0) {
+        return true;
+    }
+    return false;
 }
